12e94f447208f5c1438313e1648a6cce39da3823
Dario Lang <dario.lang@flagbit.de>
2022-07-28 16:40:53 +0200

    feat: Remove unnecessary software

--- files/load_dconf.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-busctl --user call org.gnome.Shell /org/gnome/Shell org.gnome.Shell Eval s 'Meta.restart("Restarting…")'
-sleep 15
-dconf load / < ~/.lazybytez-theme/gnome-settings.ini
-busctl --user call org.gnome.Shell /org/gnome/Shell org.gnome.Shell Eval s 'Meta.restart("Restarting…")'
-sleep 15
